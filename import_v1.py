7ba773eee124294b8778e0f863ee55bd8a5973be
TheLiveitup34 <andrew34elmopants@gmail.com>
2024-08-27 22:18:17 -0600

    Add Twitter Polls to Scraped Data

--- import_v1.py
+++ import_v1.py
@@ -57,6 +57,7 @@ def modify_tweet(tweet):
         "is_reply": tweet.is_reply,
         "href_links": [],
         "media_files": [],
+        "poll_data": [],
         "tweet_raw": tweet.text,
         "tweet_parsed": ""
     }
@@ -127,6 +128,26 @@ def modify_tweet(tweet):
                 print(f"{Fore.RED}Failed to Modify Reply Tweet for the following reason: {Fore.YELLOW}{e}{Fore.WHITE}")
                 if "Rate limit exceeded" in str(e):
                     exit()
+        # Checks if tweet Poll exists in tweet
+    if tweet.pool != None:
+        data_tweet["poll_data"]["id"] = tweet.pool.id
+        data_tweet["poll_data"]["name"] = tweet.pool.name
+        data_tweet["poll_data"]["choices"] = []
+        # Gets Poll Options
+        for choices in tweet.pool.choices:
+           data_tweet["poll_data"]["choices"].append({
+               "name": choices.name, 
+                "value": choices.name, 
+                "key": choices.key, 
+                "counts": choices.counts
+            })
+        data_tweet["poll_data"]["end_time"] = tweet.pool.end_time
+        data_tweet["poll_data"]["last_updated_time"] = tweet.pool.last_updated_time
+        data_tweet["poll_data"]["duration"] = tweet.pool.duration
+        data_tweet["poll_data"]["user_ref"] = []
+        for user_ref in tweet.pool.user_ref:
+            data_tweet["poll_data"]["user_ref"].append(user_ref.username)
+        data_tweet["poll_data"]["is_final"] = tweet.pool.is_final
 
     # Saves the file in the folder in scraped/USER/media/TWEET_ID/TWEET_ID.json
     f = open(path_name + "media" + os.sep + tweet.id + os.sep + tweet.id + ".json", "w")
