14de5c1241fa3defbed03287d52657d25a940c25
Tyviebrock <stevenrockss13@gmail.com>
2025-05-16 20:27:12 -0400

    Update configs.py

--- configs.py
+++ configs.py
@@ -13,6 +13,7 @@ It includes the following settings:
 - GrabRetweetedBy: Whether to grab the users that retweeted the tweet.
 - GrabRepliedToTweet: Whether to grab the tweet it replied to.
 - GrabReplies: Whether to grab replies from tweets.
+- GrabHiddenReplies: Whether to grab hidden replies from tweets.
 - GrabQuoteRetweets: Whether to grab quote retweets from tweets.
 
 ============================
